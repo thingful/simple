8c0fab02325ad52ad3f1dc4eb551a787efc61a0f
Sam Mulube <sam@thingful.net>
2017-06-30 12:34:20 +0100

    Switch growser to be a simple HTTP server using Goji

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
+
+	goji "goji.io"
+	"goji.io/pat"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "name")
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
 func main() {
-	resp, err := http.Get("https://google.com")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Length:", len(body))
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/hello/:name"), hello)
+
+	http.ListenAndServe("localhost:8000", mux)
 }
